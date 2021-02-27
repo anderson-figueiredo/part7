7bde6f37491826ff3f68fa14b5517524dfb93b40
Gminic <44212860+Gminic@users.noreply.github.com>
2021-02-27 22:45:24 +0000

    Update sketch.js
    Added extra scenery, improved some bits and bobs, but... found a bug, if the ghost kills you twice and them you fall from the cliff the game gets stuck.

--- sketch.js
+++ sketch.js
@@ -16,13 +16,13 @@ var mountains;
 var trees_x;
 var canyons;
 var collectables;
+var platforms;
+var enemies;
+var flowers;
 
 var game_score;
 var flagpole;
 var lives;
-var enemies;
-
-var platforms;
 
 var jumpSound;
 var appleCrunch;
@@ -32,11 +32,12 @@ var fallingSound;
 var gameOver;
 var gameSong;
 
+var gameFont;
+
 function preload()
 {
     soundFormats('mp3','wav');
     
-    //load your sounds here
     jumpSound = loadSound('assets/187024__lloydevans09__jump2.wav');
     jumpSound.setVolume(0.2);
     
@@ -44,7 +45,7 @@ function preload()
     appleCrunch.setVolume(0.5);
     
     ghostSound = loadSound('assets/490515__staudio__ghostguardian-attack-01.wav');
-    ghostSound.setVolume(0.5);
+    ghostSound.setVolume(0.1);
     
     levelCompleted = loadSound('assets/397355__plasterbrain__tada-fanfare-a.mp3');
     levelCompleted.setVolume(0.1)
@@ -56,7 +57,9 @@ function preload()
     gameOver.setVolume(0.05)
     
     gameSong = loadSound('assets/223898__theblockofsound235__the-entertainer-digital-ii-chime-song-6.wav');
-    gameSong.setVolume(0.02)  ;
+    gameSong.setVolume(0.02);
+    
+    gameFont = loadFont('assets/DotGothic16/DotGothic16-Regular.ttf');
 }
 
 
@@ -65,6 +68,9 @@ function setup()
 	createCanvas(1024, 576);
 	floorPos_y = height * 3/4;
     
+    // Background music
+    gameSong.loop();
+
     // Initialise lives.
     lives = 3;    
     startGame();
@@ -78,14 +84,21 @@ function draw()
     background(100, 155, 255); // fill the sky blue
 
 	noStroke();
-	fill(0,155,0);
-	rect(0, floorPos_y, width, height/4); // draw some green ground
+   	
+    fill(225, 169, 95); // draw some earth ground
+    rect(0, floorPos_y, width, height/4); 
+    fill(255, 155, 0); // draw some yellow ish ground
+    rect(0, floorPos_y, width, height/20);
+    fill(0, 155, 0);  // draw some green ground
+    rect(0, floorPos_y, width, height/30); 
 
+
+    
     push();
     translate(scrollPos, 0);
-    
-	// Draw clouds.
-    drawClouds();
+       
+    // Draw Clouds
+    clouds.drawAndUpdateClouds();
     
 	// Draw mountains.
     drawMountains();
@@ -116,31 +129,44 @@ function draw()
         platforms[i].draw();
     }
     
+    // Draw flowers
+    for(var i = 0; i < flowers.length; i++)
+    {
+        flowers[i].draw();
+    }
+    
     // Draw enemies
     for(var i = 0; i < enemies.length; i++)
     {
         enemies[i].draw();
         
         var isContact = enemies[i].checkContact(gameChar_world_x, gameChar_y);
-        if(isContact)
+        if(isContact || gameChar_world_x < -450)
         {
             if(lives > 0)
             {
                 lives -= 1;
                 ghostSound.play();
                 /*Adicionei a música no contato com o fantasma para tocar quando zerar a vida*/
-                if(!lives) {
-                    ghostSound.stop;
-                    gameSong.stop()
-                    gameOver.isPlaying() || gameOver.play()
-               }
+                    if(!lives) 
+                    {
+                        gameSong.stop();
+                        gameOver.isPlaying() || gameOver.play();
+                    }
                 startGame();
                 break;
             }
         }
     }
-    
+
+
+    // Render flagpole
     renderFlagpole();
+
+    // Draw demon
+    drawDeathAngel();
+
+
     pop();
     
     // Check if player went down the cliff.
@@ -151,53 +177,58 @@ function draw()
     {
         for(var i = 0; i < lives; i++)
         {
-/*       // this is the hearts: 
-            x = 40 + (i * 25)
-             y = 42
-             size = 18
-            
-            strokeWeight(1.2)
-            stroke(145, 29, 29);
-            fill(255, 0, 0);
-        
-            beginShape();
-            vertex(x, y);
-            bezierVertex(x - size / 2, y - size / 2, x - size,  y + size / 3, x, y + size);
-            bezierVertex(x + size, y + size / 3, x + size / 2,  y - size / 2, x, y);  
-            endShape(CLOSE);
-*/
-        //hat?
+            //hats left
             fill(0);
-            rect(28 + ([i] * 28), 42, 18, 15);
-            rect(24 + ([i] * 28), 57, 26, 2.3, 30);
+            rect(50 + ([i] * 30), 50, 18, 15);
+            rect(46 + ([i] * 30), 65, 26, 2.3, 30);
         }
     }
 
     // Game score.
-    textSize(16);
+    textFont(gameFont);
+    textSize(20);
     textAlign(LEFT);
     fill(255);
+    stroke(0);
+    strokeWeight(1);
+    text("Score: " + game_score + "/15", 35, 30);
     noStroke();
-    text("Score: " + game_score, 35, 30);
     
     // Game over message.
     if(lives < 1)
     {
-        textSize(24);
+        stroke(0);
+        strokeWeight(2);
+        rect(width/4, height/3, width/2, height/4);
+        noStroke();
+        
         textAlign(CENTER, CENTER);
-        text("Game over. Press space to continue.", width/2, height/2);
+        fill(0);
+        textSize(32);
+        text("Game over", width/2, height/2 - 50);
+        textSize(24);
+        text("Press space to play again", width/2, height/2 - 10);
     }
      
     // Level complete message.
     if(flagpole.isReached)
     {
+        stroke(0);
+        strokeWeight(2);
+        rect(width/4, height/3, width/2, height/4);
+        noStroke();
+        
         textSize(24);
         textAlign(CENTER, CENTER);
-        text("Level complete. Press space to continue.", width/2, height/2);
+        fill(0);
+        textSize(32);
+        text("Level complete", width/2, height/2 - 50);
+        textSize(24);
+        text("Press space to play again", width/2, height/2 - 10);
 
     }
     
-    if(! flagpole.isReached)
+    if(!flagpole.isReached)
     {
         checkFlagpole();
     }
@@ -247,7 +278,7 @@ function draw()
             }
         }
               
-        if(! isContact)
+        if(!isContact)
         {
             gameChar_y += 2;
             isFalling = true;
@@ -279,19 +310,20 @@ function keyPressed()
         isRight = true;
     }
     
-    /*mudei o código do w*/ 
-    if(key == 'w' || key == 'W' || keyCode === 87)
+    if(key == 'w' || key == 'W' || keyCode == 32)
     {  
-        if(!isFalling && !isPlummeting && lives > 0)
+        if(!isFalling && !isPlummeting && lives > 0 && !flagpole.isReached)
         {
             gameChar_y -= 100;
             jumpSound.play();
         }
     }
-	
-    /*adicionei o código do espaço com validação das vidas restantes*/ 
-    if(key == ' ' || keyCode == 32) {
-        if(!lives) {
+    
+    //restart the game pressing space
+    if(!lives || flagpole.isReached)
+    {
+        if(keyCode == 32) 
+        {
             setup();
         }
     }
@@ -435,7 +467,7 @@ function drawGameChar()
         vertex(gameChar_x + 8, gameChar_y - 13);
         vertex(gameChar_x + 16, gameChar_y + 1);
         vertex(gameChar_x + 10, gameChar_y + 3);
-        endShape(); 
+        endShape(CLOSE); 
         
 	}
 	else if(isRight)
@@ -482,7 +514,7 @@ function drawGameChar()
         vertex(gameChar_x - 8, gameChar_y - 13);
         vertex(gameChar_x - 16, gameChar_y);
         vertex(gameChar_x - 10, gameChar_y + 2);
-        endShape(); 
+        endShape(CLOSE); 
 
 	}
 	else if(isFalling || isPlummeting)
@@ -587,30 +619,6 @@ function drawGameChar()
 // Background render functions
 // ---------------------------
 
-// Function to draw cloud objects.
-function drawClouds()
-{
-    for(var i = 0; i < clouds.length; i++)
-    {
-        fill(255,255,255);
-        
-        ellipse(clouds[i].x_pos + 65 * clouds[i].size, 
-                clouds[i].y_pos * clouds[i].size,
-                clouds[i].size * 50, 
-                clouds[i].size * 50);
-        
-        ellipse(clouds[i].x_pos + 100 * clouds[i].size, 
-                clouds[i].y_pos * clouds[i].size,
-                clouds[i].size * 70, 
-                clouds[i].size * 70);
-        
-        ellipse(clouds[i].x_pos + 135 * clouds[i].size, 
-                clouds[i].y_pos * clouds[i].size, 
-                clouds[i].size * 50, 
-                clouds[i].size * 50);
-    }
-}
-
 // Function to draw mountains objects.
 function drawMountains()
 {
@@ -675,6 +683,109 @@ function drawTrees()
                  floorPos_y - 115);
     }
 }
+
+
+// Function to draw trees objects.
+function drawDeathAngel()
+{      
+
+
+        xPos = -300;
+        yPos = floorPos_y + 190
+        
+        push();
+
+        translate(xPos - 332.5, yPos - 480);
+    
+        fill(0);
+
+        // Death Angel body shape
+        beginShape();
+        vertex(145, 88);
+        vertex(160, 88);
+        curveVertex(176, 94);
+        curveVertex(180, 104);
+        vertex(184, 138);
+        vertex(195, 165);
+        vertex(203, 141);
+        vertex(210, 138);
+        vertex(210, 191);
+        vertex(194, 222);
+        vertex(187, 213);
+        vertex(185, 285);
+        curveVertex(165, 292);
+        vertex(135, 291);
+        vertex(93, 296);
+        vertex(56, 288);
+        vertex(104, 280);
+        vertex(112, 272);
+        curveVertex(136, 260);
+        vertex(144, 196);
+        vertex(144, 196);
+        vertex(130, 214);
+        vertex(118, 170);
+        vertex(127, 152);
+        vertex(130, 152);
+        vertex(136, 166);
+        vertex(138, 166);
+        vertex(150, 142);
+        vertex(152, 102);
+        vertex(140, 94);
+        curveVertex(145, 88);
+        curveVertex(160, 88);
+        endShape(CLOSE);    
+
+        // Death Angel Scythe
+        rect(125, 28, 4, 280);
+        
+        beginShape();
+        vertex(120, 36);
+        bezierVertex(120, 36, 180, 34, 212, 76);
+        bezierVertex(212, 76, 172, 56, 120, 58);
+        vertex(120, 36);
+        endShape(CLOSE);
+
+
+        // Death Angel hands
+        fill(255);
+        stroke(255);
+        strokeWeight(2);
+        strokeJoin(ROUND);
+
+        rect(123.5, 160, 6, 8);
+
+        beginShape();
+        vertex(210, 138);
+        vertex(214, 136);
+        vertex(231, 137);
+        vertex(230, 138);
+        vertex(221, 139);
+        vertex(218, 146);
+        vertex(214, 148);
+        vertex(211, 148);
+        endShape(CLOSE);
+
+        // Death Angel face
+        noStroke();
+        fill(255);
+        rect(156, 102, 21, 16, 3);
+        rect(160.5, 116, 12, 8, 3);
+        
+        fill(0);
+        ellipse(162, 109, 8);
+        ellipse(171.5, 109, 8);
+    
+        fill(255, 0, 0);
+        ellipse(163, 110, 4);
+        ellipse(172.5, 110, 4);
+
+        stroke(0);
+        strokeWeight(0.6);
+        line(163, 119, 169, 120);
+
+        pop();
+}
+
 
 // ---------------------------------
 // Canyon render and check functions
@@ -883,10 +994,7 @@ function startGame()
 {
     gameChar_x = width/2;
 	gameChar_y = floorPos_y;
-    
-    
-  //  gameSong.isPlaying() || gameSong.loop();
-	
+
     // Variable to control the background scrolling.
 	scrollPos = 0;
 
@@ -901,51 +1009,52 @@ function startGame()
     
     // Initialise game score.
     game_score = 0;
-
-	// Initialise arrays of scenery objects.
-    clouds = 
-    [
-        {x_pos: 80, y_pos: 80, size: 1.2},
-        {x_pos: 350, y_pos: 200, size: 1.0},
-        {x_pos: 600, y_pos: 130, size: 1.3},
-        {x_pos: 900, y_pos: 80, size: 0.8},
-        {x_pos: 1200, y_pos: 80, size: 1.4},
-        {x_pos: 1800, y_pos: 80, size: 1.0},
-        {x_pos: 2000, y_pos: 80, size: 0.5},
-        {x_pos: 2300, y_pos: 80, size: 1.2},
-        {x_pos: 2600, y_pos: 80, size: 1.0},
-        {x_pos: 3100, y_pos: 80, size: 0.5}
-    ];
     
     mountains = 
     [
+        {x_pos: -400, y_pos: 200},
         {x_pos: 100, y_pos: 200},
         {x_pos: 700, y_pos: 160},
         {x_pos: 1200, y_pos: 130},
-        {x_pos: 2000, y_pos: 250}
+        {x_pos: 2000, y_pos: 250},
+        {x_pos: 2300, y_pos: 130},
+        {x_pos: 2600, y_pos: 280}
 
     ];
     
     trees_x = [
+        -250, 
         100, 
         300, 
         650, 
         1000, 
+        1500,
+        1700,
         2000,
-        2500
+        2450,
+        2650,
+        3000
     ];
     
     collectables = 
     [
+        {x_pos: -350, y_pos: floorPos_y, isFound: false},
+        {x_pos: -200, y_pos: floorPos_y - 90, isFound: false},
         {x_pos: 100, y_pos: floorPos_y, isFound: false},
         {x_pos: 200, y_pos: floorPos_y - 180, isFound: false},
         {x_pos: 650, y_pos: floorPos_y - 90, isFound: false},
-     //   {x_pos: 1175, y_pos: floorPos_y - 120, isFound: false},
+        {x_pos: 935, y_pos: floorPos_y - 275, isFound: false},
         {x_pos: 1175, y_pos: floorPos_y, isFound: false},
+        {x_pos: 1400, y_pos: floorPos_y, isFound: false},
         {x_pos: 1700, y_pos: floorPos_y, isFound: false},
-        {x_pos: 2050, y_pos: floorPos_y - 90, isFound: false},
+        {x_pos: 2100, y_pos: floorPos_y - 120, isFound: false},
+        {x_pos: 2200, y_pos: floorPos_y - 180, isFound: false},
+        {x_pos: 2380, y_pos: floorPos_y - 220, isFound: false},
         {x_pos: 2500, y_pos: floorPos_y, isFound: false},
+        {x_pos: 2600, y_pos: floorPos_y - 180, isFound: false},
         {x_pos: 2850, y_pos: floorPos_y - 60, isFound: false}
+
+
     ];
     
     canyons = 
@@ -956,21 +1065,38 @@ function startGame()
         {x_pos: 2800, width: 120}
     ]; 
    
-    
+
     // platforms
     platforms = [];
     platforms.push(createPlatforms(20, floorPos_y - 100, 100));
-    platforms.push(createPlatforms(400, floorPos_y - 70, 100));
-    platforms.push(createPlatforms(2100, floorPos_y - 95, 120) );
+    platforms.push(createPlatforms(700, floorPos_y - 70 , 110));
+    platforms.push(createPlatforms(850, floorPos_y - 150, 100));
+    platforms.push(createPlatforms(2100, floorPos_y - 95, 120));
+    platforms.push(createPlatforms(2320, floorPos_y - 150, 120));
+    platforms.push(createPlatforms(2540, floorPos_y - 95, 120) );
     
     
     // enemies
     enemies = [];
-    enemies.push(new Enemy(600, floorPos_y - 10, 100));
+    enemies.push(new Enemy(600, floorPos_y - 10, 150));
     enemies.push(new Enemy(1000, floorPos_y - 10, 180));
-    enemies.push(new Enemy(1400, floorPos_y - 10, 350));    
+    enemies.push(new Enemy(1370, floorPos_y - 10, 400)); 
+    enemies.push(new Enemy(2000, floorPos_y - 10, 300));   
     enemies.push(new Enemy(2100, floorPos_y - 110, 100));
-    enemies.push(new Enemy(600, floorPos_y - 10, 100));
+    enemies.push(new Enemy(2300, floorPos_y - 10, 300));  
+    enemies.push(new Enemy(2540, floorPos_y - 110, 100));
+    
+    //flower
+    flowers = [];
+    for(var i = 0; i < 20; i++)
+    {
+        var flowerColour = color(random(150,255), random(0,100), 0);
+        flowers.push(new Flower(random(20, 3500), floorPos_y - 12, random(4,8), flowerColour));
+    }
+  
+    //Initialise Clouds
+    clouds = new CloudRespawn();
+    clouds.cloudsOnTheSky(15);
     
     //Initialise flagpole.
     flagpole = {isReached : false, x_pos: 3100, flagWidth: 80, flagHeight:48}
@@ -1064,7 +1190,7 @@ function Enemy(x, y, range)
         //ghost eye colour
         fill(0, 255, 255);
         
-        //move right and left
+        //move eyes right and left
         if(gameChar_world_x < this.currentX)
         {
             ellipse(this.currentX, this.y - 12, 3, 3);
@@ -1072,14 +1198,10 @@ function Enemy(x, y, range)
         }
         else
         {
-            //ellipse(this.currentX + 1, this.y - 12, 3, 3);
-            //ellipse(this.currentX + 10, this.y - 12, 3, 3);
-
             ellipse(this.currentX + 2, this.y - 12, 3, 3);
             ellipse(this.currentX + 11, this.y - 12, 3, 3); 
         }
         
-       
     }
     
     this.checkContact = function(gc_x, gc_y)
@@ -1091,5 +1213,124 @@ function Enemy(x, y, range)
         }
         
         return false;
+    }
+}
+
+
+function Cloud(x, y, size, speed)
+{
+    this.x = x;
+    this.y = y;
+    this.size = size;
+
+    this.speed = speed;
+    
+    this.cloudMovement = function()
+    {
+        this.x += this.speed;
+    }
+    
+    this.drawCloud = function()
+    {   
+        fill(255);
+        
+        ellipse( this.x + 65 *  this.size, 
+                 this.y *  this.size,
+                 this.size * 50, 
+                 this.size * 50);
+        
+        ellipse( this.x + 100 *  this.size, 
+                 this.y *  this.size,
+                 this.size * 70, 
+                 this.size * 70);
+        
+        ellipse( this.x + 135 *  this.size, 
+                 this.y *  this.size, 
+                 this.size * 50, 
+                 this.size * 50);
+    }
+
+}
+
+
+function CloudRespawn()
+{
+    
+    this.clouds = [];
+    this.startClouds = 0;
+
+    this.cloudsOnTheSky = function(startClouds)
+    {
+        this.startClouds = startClouds;
+        
+        //start clouds first run
+        for(var i = 0; i < startClouds; i++)
+        {
+            var nc = new Cloud(random(-500, 3100), random(70, 150), random(0.2, 1.5), random(0.1, 0.3));
+            
+            this.clouds.push(nc);
+        }
+        
+        this.drawAndUpdateClouds = function()
+        {
+            //initial draw and movement on the screen
+            var offScreenClouds = 0;
+            for(var i = this.clouds.length - 1; i >= 0; i--)
+
+            {
+                this.clouds[i].drawCloud();
+                this.clouds[i].cloudMovement();
+                if(this.clouds[i].x > flagpole.x_pos + 200)
+                {
+                    this.clouds.splice(i, 1)
+                    offScreenClouds++;
+                }
+                
+            }
+            
+            //respawn clouds that are now missing
+            if(offScreenClouds > 0)
+            {
+                for(var i = 0; i < offScreenClouds; i++)
+                {
+                    var nc = new Cloud(random(-800, -1000), random(70, 150), random(0.2, 1.5), random(0.1, 0.3));
+                    
+                    this.clouds.push(nc);
+                }
+            }
+        }
+    }
+}
+
+
+function Flower(x, y, size, color)
+{
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+    
+    this.draw = function()
+    {   
+        //flower leafs
+        stroke(0,100,0);
+        strokeWeight(0.3);
+        fill(0,150,0);
+        ellipse(this.x - this.size / 2, this.y + 10,this.size, this.size / 2);
+        ellipse(this.x + this.size / 2, this.y + 10,this.size, this.size / 2);
+        
+        strokeWeight(1);
+        line(this.x, floorPos_y, this.x, this.y);
+        noStroke();
+        //flower
+        fill(this.color);
+        ellipse(this.x - this.size / 2, this.y - this.size / 2,this.size);
+        ellipse(this.x  + this.size / 2, this.y - this.size / 2, this.size);
+        ellipse(this.x  - this.size / 2, this.y + this.size / 2, this.size);
+        ellipse(this.x  + this.size / 2, this.y + this.size / 2, this.size);
+        
+        fill(350,230,20);
+        ellipse(this.x , this.y, this.size * 0.8);
+        
     }
 }
